7212dd637be6bc3da2c300a0aad9996bed6a3e23
wbham <wbham97@naver.com>
2023-12-20 22:49:14 +0900

    [Add] - 나만의 액자 외부 저장소 동의
    - activity 관련 api level 에러로 gradle에서 target과 compileSDK를 34로 변경
    - 버튼 클릭 시 외부저장소 접근 권한 Dialog가 떠야하는데 안뜸..

--- app/src/main/java/org/shop/gallery/MainActivity.kt
+++ app/src/main/java/org/shop/gallery/MainActivity.kt
@@ -1,11 +1,72 @@
 package org.shop.gallery
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import org.shop.gallery.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
+
+        binding.loadImageButton.setOnClickListener {
+            checkPermission()
+        }
+    }
+
+    private fun checkPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                loadImage()
+            }
+
+            shouldShowRequestPermissionRationale(
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) -> {
+                showPermissionInfoDialog()
+            }
+
+            else -> {
+                requestReadExternalStorage()
+            }
+        }
+    }
+
+    private fun showPermissionInfoDialog() {
+        AlertDialog.Builder(this).apply {
+            setMessage("이미지를 가져오기 위해서, 외부 저장소 읽기 권한이 필요합니다.")
+            setNegativeButton("취소", null)
+            setPositiveButton("동의") { _, _ ->
+                requestReadExternalStorage()
+            }
+        }.show()
+    }
+
+    private fun requestReadExternalStorage() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+            REQUEST_READ_EXTERNAL_STORAGE
+        )
+    }
+
+    private fun loadImage() {
+        Log.d("MainActivity", "loadImage")
+    }
+
+    companion object {
+        const val REQUEST_READ_EXTERNAL_STORAGE = 100
     }
 }
