0177028f28480b2c1f980ae21ee3290d73abf3ad
wbham <wbham97@naver.com>
2023-12-22 02:24:46 +0900

    [Feat] - TabLayout Indicator
    - TabLayout을 ViewPager와 연결하여 현재 이미지의 위치를 표현
    - TabLayout의 배경을 dot으로 커스텀

--- app/src/main/java/org/shop/gallery/FrameActivity.kt
+++ app/src/main/java/org/shop/gallery/FrameActivity.kt
@@ -3,6 +3,7 @@ package org.shop.gallery
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.tabs.TabLayoutMediator
 import org.shop.gallery.databinding.ActivityFrameBinding
 
 class FrameActivity : AppCompatActivity() {
@@ -20,5 +21,12 @@ class FrameActivity : AppCompatActivity() {
         val frameAdapter = FrameAdapter(images)
 
         binding.viewPager.adapter = frameAdapter
+
+        /** @see
+         *  이를 통해 ViewPager와 TabLayout을 연결하고 변경된 내용이 잘 연동될 수 있도록 작업
+         */
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            binding.viewPager.currentItem = tab.position
+        }.attach()
     }
 }
